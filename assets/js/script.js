e8e580236737b64ef5ef89f5871bd1e6b85531f7
0ffpudding <jennaanderson00@gmail.com>
2022-03-26 23:39:30 -0500

    added function to set score and hiScore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,105 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Hide everything except start page
+    document.getElementById("game-over").style.display = "none";
+    document.getElementById("question").style.display = "none";
+    document.getElementById("game-over").style.display = "none";
+    document.getElementById("hi-score").style.display = "none";
+
+    // Start game
     document.getElementById("start-button").addEventListener('click', startGame);
+
+    // add questions here
+    const questions = [
+        {questionText: "Question 1", options: ["Option 1","Option 2"], answer: 1}, 
+        {questionText: "Question 2", options: ["Option 1","Option 2"], answer: 0},
+        {questionText: "Question 3", options: ["Option 1","Option 2"], answer: 2},
+        {questionText: "Question 4", options: ["Option 1","Option 2"], answer: 3},
+        {questionText: "Question 5", options: ["Option 1","Option 2"], answer: 4},
+        {questionText: "Question 6", options: ["Option 1","Option 2"], answer: 5}, 
+        {questionText: "Question 7", options: ["Option 1","Option 2"], answer: 6},
+        {questionText: "Question 8", options: ["Option 1","Option 2"], answer: 7},
+        {questionText: "Question 9", options: ["Option 1","Option 2"], answer: 8},
+        {questionText: "Question 10", options: ["Option 1","Option 2"], answer: 9}
+    ];
+
+    var seconds = 120;
+    var currentQuestion = 0;
+    var score = ((questions[i]) * 10);
+
+    function startGame() {
+        // Hide Starting Page/Text
+        document.getElementById("start-quiz").style.display = "none";
+        // Start Timer
+        setInterval(() => { timer(); }, 1000);
+        // Display Timer
+        // Start Showing Questions 
+        showQuestions();
+    }
+
+    function timer() {
+        seconds--; 
+        isGameOver();
+        updateUI();
+    }
+
+    function endTimer() {
+        if (seconds <= 0) {
+            document.getElementById("timer").style.display = "none";
+        }
+    }
+
+    function updateUI() {
+        document.getElementById("timer").innerHTML = seconds;
+    }
+
+    // add if/else function to cycle through questions
+    function showQuestions() {
+        var questionText = questions[currentQuestion].questionText; 
+        // display text in HTML
+        alert(questionText);
+        //if time left, continue with questions
+        if (/* correct answer is clicked*/ && seconds < 0) {
+            currentQuestion++;
+        } else if (/*answer is incorrect*/) {
+            timerPenalty();
+            currentQuestion++;
+        } else if (seconds <= 0) {
+            endGame();
+        }
+    }
+
+    // function to subtract time when answered incorrectly
+    function timerPenalty() {
+        seconds -= 20;
+    }
+
+    // function to end game
+    function endGame() {
+        // display "game over" page
+        endTimer();
+        document.getElementById("question").style.display = "none";
+        document.getElementById("game-over").style.display = "block"; 
+        document.getElementById("submit-initials").addEventListener("click", hiScorePage);
+    }
+
+    // function to create score and high score
+    function createHiScore() {
+        if (score > hiScore) {
+            hiScore = (score += document.getElementById("initials"));
+            localStorage.setItem(hiScore);
+        } else {
+            score += document.getElementById("initials");
+            localStorage.setItem(score);
+        }
+    }
+
+    // function to show high score page
+    function hiScorePage() {
+        document.getElementById("game-over").style.display = "none";
+        document.getElementById("hi-score").style.display = "block";
+        document.getElementById("current-hi-score").innerHTML = localStorage.getItem(hiScore);
+        document.getElementById("player-score").innerHTML = (score);
+    }   
 })
 
-// add questions here
-const questions = [
-    {questionText: "Question 1", options: ["Option 1","Option 2"], answer: 1}, 
-    {questionText: "Question 2", options: ["Option 1","Option 2"], answer: 0},
-    {questionText: "Question 3", options: ["Option 1","Option 2"], answer: 2},
-    {questionText: "Question 4", options: ["Option 1","Option 2"], answer: 3},
-    {questionText: "Question 5", options: ["Option 1","Option 2"], answer: 4},
-    {questionText: "Question 6", options: ["Option 1","Option 2"], answer: 5}, 
-    {questionText: "Question 7", options: ["Option 1","Option 2"], answer: 6},
-    {questionText: "Question 8", options: ["Option 1","Option 2"], answer: 7},
-    {questionText: "Question 9", options: ["Option 1","Option 2"], answer: 8},
-    {questionText: "Question 10", options: ["Option 1","Option 2"], answer: 9}
-];
 
-var seconds = 120;
-var currentQuestion = 0;
-var score = ((questions[i]) * 10);
-
-function startGame() {
-    // Hide Starting Page/Text
-    document.getElementById("start-quiz").style.display = "none";
-    // Start Timer
-    setInterval(() => { timer(); }, 1000);
-    // Display Timer
-    // Start Showing Questions 
-    showQuestions();
-}
-
-function timer() {
-    seconds--; 
-    isGameOver();
-    updateUI();
-}
-
-function endTimer() {
-    if (seconds <= 0) {
-        document.getElementById("timer").style.display = "none";
-    }
-}
-
-function updateUI() {
-    document.getElementById("timer").innerHTML = seconds;
-}
-
-// add if/else function to cycle through questions
-function showQuestions() {
-    var questionText = questions[currentQuestion].questionText; 
-    // display text in HTML
-    alert(questionText);
-    //if time left, continue with questions
-    if (/* correct answer is clicked*/ && seconds < 0) {
-        currentQuestion++;
-    } else if (/*answer is incorrect*/) {
-        timerPenalty();
-        currentQuestion++;
-    } else if (seconds <= 0) {
-        endGame();
-    }
-}
-
-// function to subtract time when answered incorrectly
-function timerPenalty() {
-    seconds -= 20;
-}
-
-// function to end game
-function endGame() {
-    // display "game over" page
-    endTimer();
-    document.getElementById("question").style.display = "none";
-    document.getElementById("game-over").style.display = "block"; 
-    document.getElementById("submit-initials").addEventListener("click", hiScorePage);
-}
-
-// function to create score
-// function to store score 
-function createScore() {
-    if (score > hiScore) {
-        Set(hiScore = (score += document.getElementById("initials")));
-    } else {
-        Set(score += document.getElementById("initials"));
-    }
-}
-
-// function to show high score page
-function hiScorePage() {
-    document.getElementById("game-over").style.display = "none";
-    document.getElementById("hi-score").style.display = "block";
-    document.getElementById("current-hi-score").innerHTML = fetch(hiScore);
-    document.getElementById("player-score").innerHTML = (score);
-}   
